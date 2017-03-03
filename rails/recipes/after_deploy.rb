3e3d7e867f9bd8ae7fd47d39da566c7ef5fa3dc6
Altherlex Alves <para.alves@gmail.com>
2017-03-03 18:06:00 -0300

    right way to convert json to yaml

--- rails/recipes/after_deploy.rb
+++ rails/recipes/after_deploy.rb
@@ -4,20 +4,11 @@ node[:deploy].each do |application, deploy|
 
   deploy = node[:deploy][application]
 
-  # script "install_something" do
-  #   interpreter "bash"
-  #   user "root"
-  #   cwd "/tmp"
-  #   code <<-EOH
-  #     #insert bash script
-  #   EOH
-  # end
-
   ruby_block "append_to_database_yaml" do
     block do
       require 'yaml'
       open("#{deploy[:deploy_to]}/shared/config/database.yml", 'a') { |f|
-        f << YAML.dump(deploy[:database][:append])
+        f << deploy[:database][:append].to_yaml
       }
     end
 
@@ -25,19 +16,4 @@ node[:deploy].each do |application, deploy|
       deploy[:database][:append].present? && File.directory?("#{deploy[:deploy_to]}/shared/config/")
     end
   end
-
-  # execute "echo #{deploy[:database][:append]} >> #{deploy[:deploy_to]}/shared/config/database.yml" do
-  #   user deploy[:user]
-  #   group deploy[:group]
-
-  #   only_if do
-  #     deploy[:database][:append].present? && File.directory?("#{deploy[:deploy_to]}/shared/config/")
-  #   end
-  # end
-
-  # execute "bundle exec shoryuken -C config/shoryuken.yml -L log/shoryuken.log --rails &" do
-  #   cwd current_path
-  #   environment deploy['environment_variables']
-  # end
-
 end
