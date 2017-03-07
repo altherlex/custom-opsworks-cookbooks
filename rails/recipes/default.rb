14eabb8631e9364d3824aa75a100c5ead439e1e8
Altherlex Alves <para.alves@gmail.com>
2017-03-07 12:16:26 -0300

    bundle install for all applications

--- rails/recipes/default.rb
+++ rails/recipes/default.rb
@@ -3,3 +3,17 @@ include_recipe 'deploy'
 gem_package 'bundler' do
   action :install
 end
+
+node[:deploy].each do |application, deploy|
+  
+  deploy = node[:deploy][application]
+
+  shared_path = "#{deploy[:deploy_to]}/shared"
+  execute "bundle check --path=#{shared_path}/bundle || bundle install --binstubs #{shared_path}/bin --path #{shared_path}/bundle --deployment --without development test" do
+    user config[:user]
+    group config[:group]
+    cwd current_path
+    environment config['environment_variables']
+  end
+
+end
