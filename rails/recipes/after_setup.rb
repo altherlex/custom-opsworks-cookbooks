daedcaf2374decb8a38df9159fedd03f9a95902e
Altherlex Alves <para.alves@gmail.com>
2017-03-08 17:31:18 -0300

    checking execute

--- rails/recipes/after_setup.rb
+++ rails/recipes/after_setup.rb
@@ -1,14 +1,14 @@
 node[:deploy].each do |application, deploy|
   deploy = node[:deploy][application]
 
+  Chef::Log.info("------ Starting adding rh-soft git config in ssh")
   execute 'Adding map to rh-soft git' do
-
     ssh_config=<<EOF
 
 Host git.rhsoftware.com.br
   Hostname git.rhsoftware.com.br
   User git
-  Port 18565
+  Port #{deploy['environment_variables']['GIT_PORT']}
 
 EOF
     command "echo #{ssh_config} >> ~/.ssh/config"
