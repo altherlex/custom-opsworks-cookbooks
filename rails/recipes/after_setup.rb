e8068038e8c0debe451020d1d04f0dc534e00a92
Altherlex Alves <para.alves@gmail.com>
2017-03-08 16:25:43 -0300

    setting ssh config for rhsoft git

---
+++ rails/recipes/after_setup.rb
@@ -0,0 +1,16 @@
+node[:deploy].each do |application, deploy|
+  deploy = node[:deploy][application]
+
+  execute "Adding map to rh-soft git" do
+
+    ssh_config=<<EOF
+
+Host git.rhsoftware.com.br
+  Hostname git.rhsoftware.com.br
+  User git
+  Port 18565
+
+EOF
+    command "echo #{ssh_config} >> ~/.ssh/config"
+  end
+end
