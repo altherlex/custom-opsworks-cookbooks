ecafbe21e1545cee01aaa1d701d1e74e8e7abeb1
Altherlex Alves <para.alves@gmail.com>
2017-03-17 13:17:14 -0300

    returnning trueth code

--- rails/recipes/after_deploy.rb
+++ rails/recipes/after_deploy.rb
@@ -12,11 +12,7 @@ node[:deploy].each do |application, deploy|
       }
     end
 
-    Chef::Log.info '-'*30
-    Chef::Log.info deploy[:database].inspect
-    Chef::Log.info '-'*30
-
-    # not_if "grep '#{deploy[:database][:append].keys.first.to_s}' #{deploy[:deploy_to]}/shared/config/database.yml"
+    not_if "grep '#{deploy[:database][:append].keys.first.to_s}' #{deploy[:deploy_to]}/shared/config/database.yml"
 
     only_if do
       deploy[:database][:append].present? && File.directory?("#{deploy[:deploy_to]}/shared/config/")
