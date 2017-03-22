6347ffe67df433a7bf670e4f8712bbdd688333b7
Altherlex Alves <para.alves@gmail.com>
2017-03-22 19:15:17 -0300

    condition to run precompile task

--- rails/recipes/assets.rb
+++ rails/recipes/assets.rb
@@ -3,13 +3,13 @@ include_recipe 'deploy'
 node[:deploy].each do |application, deploy|
   deploy = node[:deploy][application]
 
-  execute 'bundle exec rake assets:precompile' do
-    user deploy[:user]
-    group deploy[:group]
-    cwd "#{deploy[:deploy_to]}/current"
-    environment deploy['environment_variables'].merge('RAILS_GROUPS' => 'assets')
-
-		# not_if !deploy[:assets]
+  if deploy[:assets]
+    execute 'bundle exec rake assets:precompile' do
+      user deploy[:user]
+      group deploy[:group]
+      cwd "#{deploy[:deploy_to]}/current"
+      environment deploy['environment_variables'].merge('RAILS_GROUPS' => 'assets')
+    end
   end
 
 end
