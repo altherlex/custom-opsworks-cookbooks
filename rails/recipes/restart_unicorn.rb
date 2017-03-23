f54c8ab9a065cc088be62172f0adf279c386ba41
Altherlex Alves <para.alves@gmail.com>
2017-03-23 20:30:31 -0300

    restart unicorn

---
+++ rails/recipes/restart_unicorn.rb
@@ -0,0 +1,9 @@
+include_recipe 'deploy'
+
+node[:deploy].each do |application, deploy|
+  deploy = node[:deploy][application]
+
+  railsenv = deploy['environment_variables']['RAILS_ENV'] || 'production'
+  execute "#{deploy[:deploy_to]}/shared/scripts/unicorn restart -E #{railsenv}"
+end
+
