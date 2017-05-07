bdf8eef6bdf02e1760bb8386c8e00204b2a6a626
Altherlex Alves <para.alves@gmail.com>
2017-05-07 02:55:14 -0300

    execute rake dow:migrate for once

--- rails/recipes/dow_migrate.rb
+++ rails/recipes/dow_migrate.rb
@@ -3,7 +3,7 @@ include_recipe 'deploy'
 node[:deploy].each do |application, deploy|
   deploy = node[:deploy][application]
 
-  if deploy[:migrate]
+  if first_instance?
     execute 'bundle exec rake dow:migrate' do
       user deploy[:user]
       group deploy[:group]
