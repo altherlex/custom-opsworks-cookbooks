6f1369a73b18066e2622710790b4680559ce7453
Altherlex Alves <para.alves@gmail.com>
2017-05-18 22:49:21 -0300

    Update dow_migrate.rb

--- rails/recipes/dow_migrate.rb
+++ rails/recipes/dow_migrate.rb
@@ -3,13 +3,13 @@ include_recipe 'deploy'
 node[:deploy].each do |application, deploy|
   deploy = node[:deploy][application]
 
-  if first_instance?
+  #if first_instance?
     execute 'bundle exec rake dow:migrate' do
       user deploy[:user]
       group deploy[:group]
       cwd "#{deploy[:deploy_to]}/current"
       environment deploy['environment_variables']
     end
-  end
+  #end
 
 end
