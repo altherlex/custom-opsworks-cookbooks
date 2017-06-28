f1afe21819a22ebf83c9939e2f0222ef686f58b5
Altherlex Alves <para.alves@gmail.com>
2017-06-28 13:29:27 -0300

    Update restart_unicorn.rb

--- rails/recipes/restart_unicorn.rb
+++ rails/recipes/restart_unicorn.rb
@@ -4,6 +4,6 @@ node[:deploy].each do |application, deploy|
   deploy = node[:deploy][application]
 
   railsenv = deploy['environment_variables']['RAILS_ENV'] || 'production'
-  execute "#{deploy[:deploy_to]}/shared/scripts/unicorn restart -E #{railsenv}"
+  execute "#{deploy[:deploy_to]}/shared/scripts/unicorn restart"
 end
 
