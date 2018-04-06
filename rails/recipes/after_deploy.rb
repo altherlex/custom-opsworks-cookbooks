7cfc234f8fd71f00b2dc3135513a0d61e844a3a8
Altherlex Alves <para.alves@gmail.com>
2018-04-06 11:52:11 -0300

    Unicorn restart

--- rails/recipes/after_deploy.rb
+++ rails/recipes/after_deploy.rb
@@ -22,10 +22,17 @@ node[:deploy].each do |application, deploy|
     # end
   end
 
-  execute "./unicorn restart" do
+  execute "unicorn_stopping" do
+    user deploy[:user]
+    group deploy[:group]
+    command "ps aux | grep 'unicorn' | awk '{print $2}' | xargs sudo kill -9"
+  end
+
+  execute "unicorn_restarting" do
     user deploy[:user]
     group deploy[:group]
     cwd "#{deploy[:deploy_to]}/shared/scripts"
+    command './unicorn start'
     environment deploy['environment_variables']
   end  
 end
